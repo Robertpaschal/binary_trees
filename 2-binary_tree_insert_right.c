6a6070fe9a53241a6592598bb94de244e47eb105
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-01-31 13:16:17 +0100

    left

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,12 +10,21 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
 
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node;
+
+		if (!parent)
+			return (NULL);
+
+		new_node = binary_tree_node(parent, value);
+
+		if (!new_node)
+			return (NULL);
 
 	if (new_node == NULL)
 	{
