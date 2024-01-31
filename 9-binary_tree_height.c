830e87a7c1e6f5c0e30bf16adfe9e87ac36a5a14
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-01-31 15:43:15 +0100

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,11 +15,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
     	return (0);
 		
-	if (tree->left)
-		left_height = binary_tree_height(tree->left);
-
-	if (tree->right)
-		right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 	{
