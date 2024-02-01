d39aff7718f332d3dc67b6198951812a9e7b1070
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-02-01 12:13:45 +0100

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,7 +10,7 @@ size_t binary_tree_he(const binary_tree_t *tree)
 	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
